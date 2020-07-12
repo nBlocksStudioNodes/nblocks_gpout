7e144514ec09bb9db760fc251c8cd8a7963a464b
nikos chalikias <nikos.chalikias@gmail.com>
2020-07-12 10:44:06 +0100

    _out.mode wih pointer

--- gpout.cpp
+++ gpout.cpp
@@ -4,7 +4,7 @@
 nBlock_GPOUT::nBlock_GPOUT(PinName pinOut, const char * modeString): _out(pinOut) {
     _mode = modeString; //  a pointer to this constant string
     _out.output();  //setup as output
-    _out.mode(_mode); //set mode as PullUp/PullDown/PullNone/OpenDrain
+    _out.mode(*_mode); //set mode as PullUp/PullDown/PullNone/OpenDrain
     return;
 }
 void nBlock_GPOUT::triggerInput(uint32_t inputNumber, uint32_t value) {
